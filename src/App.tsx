58601cfa3ae8328da9ff372bcf82e438f3784c80
dedev168943 <calvin168943@gmail.com>
2023-10-27 16:35:35 +0200

    fix: base url error fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { SignInPage } from './pages'
+import { SignInPage } from 'pages'
 import './App.css'
 const App: React.FC = () => {
   return (
